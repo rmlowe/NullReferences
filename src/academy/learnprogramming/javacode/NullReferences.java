32bad43d828b734724f2568a68532b6fd4a0bf9d
Robert Lowe <robertmlowe@rmlowe.com>
2018-01-22 12:35:34 +0800

    Initial commit

---
+++ src/academy/learnprogramming/javacode/NullReferences.java
@@ -0,0 +1,10 @@
+package academy.learnprogramming.javacode;
+
+public class NullReferences {
+
+    public static void main(String[] args) {
+        String str = null;
+        str.toUpperCase();
+    }
+
+}
